caa537de49bc630691aaef15bc095daae5299fa2
Mike Lawlor <154077460+Crimson-Wizard@users.noreply.github.com>
2024-02-27 20:29:23 +0000

    add event listeners for buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+//wait for the DOM to finsih before runnig the game 
+//Get the button elements and add listeners to them 
+
+document,addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if(this.getAttribute("data-type") == "submit") {
+                alert("You Clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You Clicke ${gameType}`);
+            
+            }
+        })
+    }
+})
 function runGame() {
 
 }
